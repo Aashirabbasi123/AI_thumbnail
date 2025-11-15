915da99064eae930d2e2e27bb57fbff84a2c6cf3
HP <hp@gmail.com>
2025-11-15 17:27:00 +0500

    home page created

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ buttons.forEach(btn => {
 document.getElementById('generateBtn').addEventListener('click', () => {
   alert('Generating Thumbnails...');
 });
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+    const answer = item.querySelector(".faq-answer");
+    const arrow = item.querySelector(".arrow");
+
+    question.addEventListener("click", () => {
+
+        // Close all other items first
+        faqItems.forEach(otherItem => {
+            if (otherItem !== item) {
+                const otherAnswer = otherItem.querySelector(".faq-answer");
+                const otherArrow = otherItem.querySelector(".arrow");
+
+                otherAnswer.style.maxHeight = null;
+                otherArrow.classList.remove("rotate");
+            }
+        });
+
+        // Toggle current item
+        if (answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+            arrow.classList.remove("rotate");
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+            arrow.classList.add("rotate");
+        }
+    });
+});
+
